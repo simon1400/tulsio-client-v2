694f65bedb7b692a09287a5979cc1be49ce93408
Your Name <>
2023-01-09 20:16:51 +0100

    fixed type error in styles

--- styles/components/button.ts
+++ styles/components/button.ts
@@ -4,7 +4,7 @@ export const button = (theme: any) => ({
       fontSize: "19px",
       boxShadow: '0 3px 6px 0 rgba(0, 0, 0, 0.16)',
       fontWeight: 600,
-      textTransform: 'none',
+      // textTransform: 'none',
       height: "60px",
       minWidth: '190px',
       borderRadius: '30px',
