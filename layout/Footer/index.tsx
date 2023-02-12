a288c4b8525ebaeed2ca6e7c33b81623ddf94fba
Your Name <>
2023-02-12 21:39:40 +0100

    add robots

--- layout/Footer/index.tsx
+++ layout/Footer/index.tsx
@@ -1,57 +1,65 @@
-import { useQuery } from '@apollo/client'
-import navFooter from '../../queries/navFooter'
-import { FooterS } from './styles'
-import Newsletter from 'components/Newsletter'
-import SocialNav from 'components/SocialNav'
-import Nav from 'components/Nav'
-import { useRouter } from 'next/router'
-import { SyntheticEvent, useEffect, useState } from 'react'
-import { useTheme } from '@emotion/react'
-import { useMediaQuery } from '@mui/material'
+import { useQuery } from "@apollo/client";
+import navFooter from "../../queries/navFooter";
+import { FooterS } from "./styles";
+import Newsletter from "components/Newsletter";
+import SocialNav from "components/SocialNav";
+import Nav from "components/Nav";
+import { useRouter } from "next/router";
+import { SyntheticEvent, useEffect, useState } from "react";
+import { useTheme } from "@emotion/react";
+import { useMediaQuery } from "@mui/material";
 
 const Footer = () => {
-
   const { loading, data } = useQuery(navFooter);
 
-  const router = useRouter()
+  const router = useRouter();
 
   const [value, setValue] = useState<number>(0);
 
   useEffect(() => {
-    if(data?.navigation) {
-      if(data.navigation.data.attributes.footer.item.length) {
-        const idx = data.navigation.data.attributes.footer.item.findIndex((el: any) => el.link === router.asPath)
-        setValue(idx >= 0 ? idx : false)
+    if (data?.navigation) {
+      if (data.navigation.data.attributes.footer.item.length) {
+        const idx = data.navigation.data.attributes.footer.item.findIndex(
+          (el: any) => el.link === router.asPath
+        );
+        setValue(idx >= 0 ? idx : false);
       }
     }
-  }, [data, router])
+  }, [data, router]);
 
-  const theme = useTheme()
-  const md = useMediaQuery(theme.breakpoints.down("md"))
+  const theme = useTheme();
+  const md = useMediaQuery(theme.breakpoints.down("md"));
 
-  if(loading) {
-    return <></>
+  if (loading) {
+    return <></>;
   }
 
-  const transformData = data.navigation.data.attributes.footer.item.map((item: any, idx: number) => ({
-    title: item.name,
-    slug: '/'+item.link
-  }))
+  const transformData = data.navigation.data.attributes.footer.item.map(
+    (item: any, idx: number) => ({
+      title: item.name,
+      slug: "/" + item.link,
+    })
+  );
 
   const handleNav = (e: SyntheticEvent, idx: number) => {
     setValue(idx);
-    router.push(transformData[idx].slug)
+    router.push(transformData[idx].slug);
   };
 
   return (
     <>
       <Newsletter />
       <FooterS>
-        <Nav data={transformData} handle={handleNav} value={value} orientation={md ? "vertical" : "horizontal"}/>
+        <Nav
+          data={transformData}
+          handle={handleNav}
+          value={value}
+          orientation={md ? "vertical" : "horizontal"}
+        />
         <SocialNav data={data} loading={loading} type="socNav" />
       </FooterS>
     </>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
