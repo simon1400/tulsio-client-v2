d3db570b4b5bc962bba3dbe99cadf7e7ca227c11
Your Name <>
2023-02-17 17:00:47 +0100

    defer script gtm

--- layout/Page/index.tsx
+++ layout/Page/index.tsx
@@ -135,7 +135,7 @@ const Page: FC<IPage> = ({ children, className = "", id = "" }) => {
       </Head>
 
       {/*<!-- Google Tag Manager -->*/}
-      <Script id="gtm">
+      <Script strategy="afterInteractive" id="gtm">
         {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.rel='preconnect';j.src=
