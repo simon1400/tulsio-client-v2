9462ba2e722019a77d18977d678c8f1743fd6e8c
Your Name <>
2023-07-18 15:08:41 +0200

    sdfgff

--- components/Anchors/styles.ts
+++ components/Anchors/styles.ts
@@ -9,19 +9,31 @@ export const AnchorsS = styled.div(({theme}) => `
         display: none;
       }
       a{
-       text-decoration: none;
-       font-size: 20px;
-       font-weight: 600;
-       transition: all .2s ease;
-       &:hover{
+        text-decoration: none;
+        font-size: 20px;
+        line-height: 1.84;
+        font-weight: 600;
+        transition: all .2s ease;
+        &:hover{
         color: ${theme.palette.primary.main};
-       }
-       svg{
-        fill: ${theme.palette.primary.main};
-        transform: rotate(-45deg);
-        margin-top: 2px;
-        margin-right: 10px;
-       }
+        }
+        svg{
+          fill: ${theme.palette.primary.main};
+          transform: rotate(-45deg);
+          margin-top: 2px;
+          margin-right: 10px;
+        }
+        ${theme.breakpoints.down('xl')} {
+          font-size: 19px;
+        },
+        ${theme.breakpoints.down('lg')} {
+          font-size: 18px;
+          line-height: 1.83;
+        },
+        ${theme.breakpoints.down('md')} {
+          font-size: 17px;
+          line-height: 1.71;
+        },
       }
     }
   }
--- styles/typography/baseBody.ts
+++ styles/typography/baseBody.ts
@@ -2,55 +2,22 @@ import { paragraph } from "./paragraph";
 
 export const baseBody = (theme: any) => ({
   color: "rgba(255, 255, 255, .85)",
-  fontSize: "19px",
+  fontSize: "20px",
   lineHeight: "1.84",
   fontFamily: theme.typography.fontFamily,
   "&::selection": {
     backgroundColor: theme.palette.primary.main,
   },
   p: paragraph(theme),
-  [theme.breakpoints.down('xxl')]: {
-    fontSize: '17px',
-    lineHeight: "1.82",
-    // ul: {
-    //   li: {
-    //     "&:before": {
-    //       top: "13px"
-    //     }
-    //   }
-    // }
+  [theme.breakpoints.down('xl')]: {
+    fontSize: '19px',
   },
   [theme.breakpoints.down('lg')]: {
-    fontSize: '16px',
-    lineHeight: 1.81,
-    // ul: {
-    //   li: {
-    //     "&:before": {
-    //       top: "13px"
-    //     }
-    //   }
-    // }
+    fontSize: '18px',
+    lineHeight: 1.83,
   },
   [theme.breakpoints.down('md')]: {
-    fontSize: '14px',
-    lineHeight: 1.79,
-    // ul: {
-    //   li: {
-    //     "&:before": {
-    //       top: "13px"
-    //     }
-    //   }
-    // }
+    fontSize: '17px',
+    lineHeight: 1.71,
   },
-  [theme.breakpoints.down('sm')]: {
-    fontSize: '13px',
-    lineHeight: 1.77,
-    // ul: {
-    //   li: {
-    //     "&:before": {
-    //       top: "13px"
-    //     }
-    //   }
-    // }
-  }
 })
