0ce813341b5c502bc92fb747c348c621def22f92
Your Name <>
2024-08-26 11:26:23 +0200

    sdfdsf

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   apps : [{
-    name   : "Tulsio client v2",
+    name   : "Tulsio client",
     script : "npm start",
     env_production: {}
   }],
@@ -11,7 +11,7 @@ module.exports = {
       host : ['89.221.216.23'],
       ref  : 'origin/main',
       repo : 'git@github.com:simon1400/tulsio-client-v2.git',
-      path : '/var/www/tulsio/client-v2',
+      path : '/home/dimi/app/tulsio/client',
       'post-deploy' : 'npm i && npm run build && pm2 reload ecosystem.config.js --env production',
     }
   }
