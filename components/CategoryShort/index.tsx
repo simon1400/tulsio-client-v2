f5d035d72ea1858a42b0b0ef41d9f256af6a9c93
Dmytro Pechunka <pechunka11@gmail.com>
2025-02-18 22:21:28 +0100

    optimize, rewrite refactor fetch articles data

--- components/CategoryShort/index.tsx
+++ components/CategoryShort/index.tsx
@@ -1,15 +1,17 @@
-import type { FC } from 'react'
+import { FC, useEffect, useState } from 'react';
+import { Typography } from '@mui/material';
+import DOMPurify from 'isomorphic-dompurify';
 
-import { Typography } from '@mui/material'
-import DOMPurify from 'isomorphic-dompurify'
-
-import { CategoryDescriptionS } from './styles'
+import { CategoryDescriptionS } from './styles';
 
 interface ICategoryShort {
-  data: any
-  staticBlock?: boolean
-  descriptionBlock?: boolean
-  removeHover?: boolean
+  data: {
+    title?: string;
+    description?: string;
+  };
+  staticBlock?: boolean;
+  descriptionBlock?: boolean;
+  removeHover?: boolean;
 }
 
 const CategoryShort: FC<ICategoryShort> = ({
@@ -18,24 +20,36 @@ const CategoryShort: FC<ICategoryShort> = ({
   removeHover = false,
   descriptionBlock = false,
 }) => {
+  const [sanitizedDescription, setSanitizedDescription] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (data?.description) {
+      setSanitizedDescription(DOMPurify.sanitize(data.description));
+    }
+  }, [data?.description]);
+
+  if (!data?.title && !data?.description) return null; // Защита от рендера пустого блока
+
   return (
     <CategoryDescriptionS
-      removeHover={removeHover}
-      staticBlock={staticBlock}
-      descriptionBlock={descriptionBlock}
+      removeHover={!!removeHover}
+      staticBlock={!!staticBlock}
+      descriptionBlock={!!descriptionBlock}
     >
-      <div className={'img-wrap'} />
-      <div className={'content-wrap-art'}>
-        <Typography marginBottom={staticBlock ? 5 : 0} variant={staticBlock ? 'h1' : 'h2'}>
-          {data.title}
-        </Typography>
-        <Typography
-          variant={'body2'}
-          dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(data.description) }}
-        />
+      <div className="img-wrap" />
+      <div className="content-wrap-art">
+        {data.title && (
+          <Typography marginBottom={staticBlock ? 5 : 0} variant={staticBlock ? 'h1' : 'h2'}>
+            {data.title}
+          </Typography>
+        )}
+
+        {sanitizedDescription && (
+          <Typography variant="body2" dangerouslySetInnerHTML={{ __html: sanitizedDescription }} />
+        )}
       </div>
     </CategoryDescriptionS>
-  )
-}
+  );
+};
 
-export default CategoryShort
+export default CategoryShort;
